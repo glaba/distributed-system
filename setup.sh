ca09cc2bfcf6611667d392dcd91aaa47c4f31085
Probasco <lawsonp2@fa19-cs425-g60-01.cs.illinois.edu>
2019-09-30 00:32:14 -0500

    cleaning up setup script slightly

--- setup.sh
+++ setup.sh
@@ -10,14 +10,15 @@ SCRIPTS_SRC=$DIR"/.scripts"
 SCRIPTS_DEST=$HOME"/.scripts"
 
 # make .ssh dir if not already, then cp authorized_keys
-mkdir ~/.ssh
+[ -d ~/.ssh ] || mkdir ~/.ssh
 cp authorized_keys ~/.ssh
 
 # create machine.i.log
 echo -n '' >> ~/machine.i.log
 
 # copy .scripts and add to path
-cp -r $SCRIPTS_SRC $SCRIPTS_DEST
+[ -d $SCRIPTS_DEST ] || mkdir $SCRIPTS_DEST
+cp -r $SCRIPTS_SRC/* $SCRIPTS_DEST
 if [[ ":$PATH:" != *":$SCRIPTS_DEST:"* ]]; then
   echo export PATH=\"$SCRIPTS_DEST:\$PATH\" >> ~/.bashrc
 fi
