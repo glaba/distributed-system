f0fbad3141f3f3b939572c641f2b28c761dfa859
Probasco <lawsonp2@fa19-cs425-g60-01.cs.illinois.edu>
2019-10-30 03:04:50 -0500

    some preliminary structure for tcp connections - probably going to change to an epoll implementation tomorrow if we REALLY care about leaving connections open

---
+++ mp3/inc/tcp.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+class tcp_utils {
+public:
+    /**
+     * Read the first four bytes from socket and transform it into ssize_t
+     * Returns the size of the incomming message, 0 if socket is disconnected, -1 on failure
+     */
+    ssize_t get_message_size(int socket);
+
+    /**
+     * Writes the bytes of size to the socket
+     * Returns the number of bytes successfully written, 0 if socket is disconnected, or -1 on failure
+     */
+    ssize_t write_message_size(size_t size, int socket);
+
+    /**
+     * Attempts to read all count bytes from socket into buffer.
+     * Returns the number of bytes read, 0 if socket is disconnected, or -1 on failure.
+     */
+    ssize_t read_all_from_socket(int socket, char *buffer, size_t count);
+
+    /**
+     * Attempts to write all count bytes from buffer to socket.
+     * Returns the number of bytes written, 0 if socket is disconnected, or -1 on failure.
+     */
+    ssize_t write_all_to_socket(int socket, const char *buffer, size_t count);
+};
+
+class tcp_server_intf : tcp_utils {
+public:
+    /**
+     * sets up server to receive connections on the given port
+     */
+	virtual void setup_server(int port) = 0;
+    /**
+     * tears down to server
+     */
+	virtual void tear_down_server() = 0;
+    /**
+     * accepts a connection on the server fd
+     * returns the socket to the accepted connection
+     */
+	virtual int accept_connection() = 0;
+    /**
+     * cleans up the connection on the given socket
+     */
+	virtual void close_connection(int client_socket) = 0;
+    /**
+     * reads the specified number of bytes from the given socket
+     * returns number of bytes read, 0 on socket disconnect, and -1 on failure
+     */
+	virtual ssize_t read_from_client(int client, char *buffer, size_t count) = 0;
+    /**
+     * writes the specified number of bytes to the given socket
+     * returns number of bytes written, 0 on socket disconnect, and -1 on failure
+     */
+	virtual ssize_t write_to_client(int client, const char *buffer, size_t count) = 0;
+};
+
+class tcp_server : tcp_server_intf {
+public:
+	void setup_server(int port);
+	void tear_down_server();
+	int accept_connection();
+	void close_connection(int client_socket);
+	ssize_t read_from_client(int client, char *buffer, size_t count);
+	ssize_t write_to_client(int client, const char *buffer, size_t count);
+private:
+    int server_fd;
+};
+
+
+class tcp_client_intf : tcp_utils {
+public:
+    /**
+     * creates client connection to a server
+     * returns socket to server
+     */
+	virtual int setup_connection(string host, string port) = 0;
+    /**
+     * reads the specified number of bytes from the given socket
+     * returns number of bytes read, 0 on socket disconnect, and -1 on failure
+     */
+	virtual ssize_t read_from_server(int socket, char *buffer, size_t count) = 0;
+    /**
+     * writes the specified number of bytes to the given socket
+     * returns number of bytes written, 0 on socket disconnect, and -1 on failure
+     */
+	virtual ssize_t write_to_server(int socket, const char *buffer, size_t count) = 0;
+    /**
+     * cleans up the connection on the given socket
+     */
+	virtual void close_connection(int socket) = 0;
+};
+
+class tcp_client : tcp_client_intf {
+public:
+	int setup_connection(string host, string port);
+	ssize_t read_from_server(int socket, char *buffer, size_t count);
+	ssize_t write_to_server(int socket, const char *buffer, size_t count);
+	void close_connection(int socket);
+}
