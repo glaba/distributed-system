96bfe3ec38e792c3b748c1e6047772534f87ba54
Probasco <lawsonp2@fa19-cs425-g60-01.cs.illinois.edu>
2019-11-02 18:49:38 -0500

    sdfs_client

--- mp3/inc/sdfs_client.h
+++ mp3/inc/sdfs_client.h
@@ -1,35 +1,44 @@
 #include "tcp.h"
+#include <fstream>
+
+#define SDFS_ACK_MSG "OK"
+#define SDFS_SUCCESS_MSG "SUCCESS"
+#define SDFS_FAILURE_MSG "FAILURE"
 
 class sdfs_client : public tcp_client {
 public:
-    sdfs_client(std::string master_hostname, std::string fs_port) :
-        master_hostname(master_hostname), fs_port(fs_port) {}
+    sdfs_client(std::string master_hostname, std::string fs_port, tcp_client client) :
+        master_hostname(master_hostname), fs_port(fs_port), client(client) {}
     /*
      * handles the put operation with the given arguments
      * returns 0 on success, -1 on failure
      **/
-    int put_operation(std::string local_filename, std::string sdfs_filename);
+    std::string put_operation(std::string local_filename, std::string sdfs_filename);
     /*
      * handles the get operation with the given arguments
      * returns 0 on success, -1 on failure
      **/
-    int get_operation(std::string local_filename, std::string sdfs_filename);
+    std::string get_operation(std::string local_filename, std::string sdfs_filename);
     /*
      * handles the delete operation with the given argument
      * returns 0 on success, -1 on failure
      **/
-    int delete_operation(std::string sdfs_filename);
+    std::string delete_operation(std::string sdfs_filename);
     /*
      * handles the ls operation with the given argument
      * returns 0 on success, -1 on failure
      **/
-    int ls_operation(std::string sdfs_filename);
+    std::string ls_operation(std::string sdfs_filename);
     /*
      * handles the store operation
      * returns 0 on success, -1 on failure
      **/
-    int store_operation();
+    std::string store_operation();
 private:
+    int send_file_over_socket(int socket, std::string filename);
+    int recv_file_over_socket(int socket, std::string filename);
+
     std::string master_hostname;
     std::string fs_port;
+    tcp_client client;
 };
--- mp3/src/sdfs_client.cpp
+++ mp3/src/sdfs_client.cpp
@@ -1,21 +1,95 @@
 #include "sdfs_client.h"
 
-int sdfs_client::put_operation(std::string local_filename, std::string sdfs_filename) {
+std::string sdfs_client::put_operation(std::string local_filename, std::string sdfs_filename) {
+    std::string put_msg = "put " + sdfs_filename;
+
+    // connect and write put request to server
+    int socket;
+    if ((socket = client.setup_connection(master_hostname, fs_port)) == -1) return SDFS_FAILURE_MSG;
+    if (client.write_to_server(socket, put_msg) == -1) return SDFS_FAILURE_MSG;
+
+    // read server response - proceed if server responded "OK"
+    std::string read_ret = client.read_from_server(socket);
+    if (read_ret != "OK") return SDFS_FAILURE_MSG;
+
+    // if server responded "OK", send the file over the socket
+    if (send_file_over_socket(socket, local_filename) == -1) return SDFS_FAILURE_MSG;
+
+    // read the server response and return (hopefully the response is SDFS_SUCCESS_MSG)
+    read_ret = client.read_from_server(socket);
+    if (read_ret == "") return SDFS_FAILURE_MSG;
+    return read_ret;
+}
+
+std::string sdfs_client::get_operation(std::string local_filename, std::string sdfs_filename) {
+    std::string get_msg = "get " + sdfs_filename;
+
+    // connect and write get request to server
+    int socket;
+    if ((socket = client.setup_connection(master_hostname, fs_port)) == -1) return SDFS_FAILURE_MSG;
+    if (client.write_to_server(socket, get_msg) == -1) return SDFS_FAILURE_MSG;
+
+    // read server response - proceed if server responded "OK"
+    std::string read_ret = client.read_from_server(socket);
+    if (read_ret != "OK") return SDFS_FAILURE_MSG;
+
+    // if server responded "OK", recv the file over the socket
+    if (recv_file_over_socket(socket, local_filename) == -1) return SDFS_FAILURE_MSG;
+
+    return SDFS_SUCCESS_MSG;
+}
+
+std::string sdfs_client::delete_operation(std::string sdfs_filename) {
+    std::string del_msg = "delete " + sdfs_filename;
+
+    // connect and write get request to server
+    int socket;
+    if ((socket = client.setup_connection(master_hostname, fs_port)) == -1) return SDFS_FAILURE_MSG;
+    if (client.write_to_server(socket, del_msg) == -1) return SDFS_FAILURE_MSG;
+
+    // read server response - proceed if server responded "OK"
+    std::string read_ret = client.read_from_server(socket);
+    if (read_ret != "OK") return SDFS_FAILURE_MSG;
+
+    return read_ret;
+}
+
+std::string sdfs_client::ls_operation(std::string sdfs_filename) {
+    std::string ls_msg = "ls " + sdfs_filename;
+
+    // connect and write get request to server
+    int socket;
+    if ((socket = client.setup_connection(master_hostname, fs_port)) == -1) return SDFS_FAILURE_MSG;
+    if (client.write_to_server(socket, ls_msg) == -1) return SDFS_FAILURE_MSG;
+
+    // read server response - proceed if server responded "OK"
+    std::string read_ret = client.read_from_server(socket);
+    if (read_ret != "OK") return SDFS_FAILURE_MSG;
+
+    // if server responded "OK", read the ls results over the socket
+    if ((read_ret = client.read_from_server(socket)) == "") return SDFS_FAILURE_MSG;
+    return read_ret;
+}
+
+std::string sdfs_client::store_operation() {
+    // this one is a local operation
     return 0;
 }
 
-int sdfs_client::get_operation(std::string local_filename, std::string sdfs_filename) {
-    return 0;
+int sdfs_client::send_file_over_socket(int socket, std::string filename) {
+    // https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+    std::ifstream t(filename);
+    std::string file_str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+
+    return client.write_to_server(socket, file_str);
 }
 
-int sdfs_client::delete_operation(std::string sdfs_filename) {
-    return 0;
-}
+int sdfs_client::recv_file_over_socket(int socket, std::string filename) {
+    // https://stackoverflow.com/questions/15388041/how-to-write-stdstring-to-file
+    std::ofstream file(filename);
+    std::string read_ret = client.read_from_server(socket);
+    if (read_ret == "") return -1;
 
-int sdfs_client::ls_operation(std::string sdfs_filename) {
-    return 0;
-}
-
-int sdfs_client::store_operation() {
+    file << read_ret;
     return 0;
 }
